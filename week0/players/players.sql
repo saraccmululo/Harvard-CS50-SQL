45378a5075d77e25e2dbf05f29b483cc86f5c34d
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-11-11 11:34:59 -0600

    fixed errors

--- week0/players/players.sql
+++ week0/players/players.sql
@@ -15,13 +15,13 @@ SELECT "first_name", "last_name" FROM "players" WHERE "birth_country" != 'USA' O
 SELECT "first_name", "last_name" FROM "players" WHERE "bats" = 'R' ORDER BY "first_name", "last_name";
 
 --In 6.sql, write a SQL query to return the first name, last name, and debut date of players born in Pittsburgh, Pennsylvania (PA). Sort the results first by debut date—from most recent to oldest—then alphabetically by first name, followed by last name.
-SELECT "first_name", "last_name", "debut" FROM "players" WHERE "birth_city" = 'Pittsburgh' ORDER BY "debut" DESC, "first_name" ASC, "last_name" ASC;
+SELECT "first_name", "last_name", "debut" FROM "players" WHERE "birth_city" = 'Pittsburgh' AND "birth_state" = 'PA' ORDER BY "debut" DESC, "first_name" ASC, "last_name" ASC;
 
 --In 7.sql, write a SQL query to count the number of players who bat (or batted) right-handed and throw (or threw) left-handed, or vice versa.
-SELECT COUNT("first_name") FROM "players" WHERE "bats" = 'R' AND "throws" = 'L' OR "bats" = 'L' AND "throws" = 'R';
+SELECT COUNT("first_name") FROM "players" WHERE ("bats" = 'R' AND "throws" = 'L') OR ("bats" = 'L' AND "throws" = 'R');
 
 --In 8.sql, write a SQL query to find the average height and weight, rounded to two decimal places, of baseball players who debuted on or after January 1st, 2000. Return the columns with the name “Average Height” and “Average Weight”, respectively.
-SELECT ROUND(AVG("height"), 2) AS "Average Height", ROUND(AVG("weight"), 2) AS "Average Weight" FROM "players" WHERE "debut" > '2000-01-01';
+SELECT ROUND(AVG("height"), 2) AS "Average Height", ROUND(AVG("weight"), 2) AS "Average Weight" FROM "players" WHERE "debut" >= '2000-01-01';
 
 --In 9.sql, write a SQL query to find the players who played their final game in the MLB in 2022. Sort the results alphabetically by first name, then by last name.
 SELECT "first_name", "last_name" FROM "players" WHERE "final_game" BETWEEN '2022-01-01' AND '2022-12-31' ORDER BY "first_name", "last_name";
@@ -30,4 +30,4 @@ SELECT "first_name", "last_name" FROM "players" WHERE "final_game" BETWEEN '2022
 --Involve at least condition, using WHERE
 --Sort by at least one column using ORDER BY
 
-SELECT ("first_name") AS "First Name", "birth_year" AS "Year of Birth" FROM "players" WHERE "birth_year" BETWEEN '1800' AND '1900' ORDER BY "birth_year";
+SELECT "first_name" AS "First Name", "birth_year" AS "Year of Birth" FROM "players" WHERE "birth_year" BETWEEN 1800 AND 1900 ORDER BY "birth_year";
